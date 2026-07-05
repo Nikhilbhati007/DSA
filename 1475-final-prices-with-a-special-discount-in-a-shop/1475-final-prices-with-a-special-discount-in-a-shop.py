95dde9c225fa1ed4b8383c0d11156a8294b566f3
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-07-05 14:23:29 +0530

    Time: 3 ms (76.49%), Space: 12.5 MB (12.17%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,18 +2,13 @@ class Solution(object):
     def finalPrices(self, prices):
         n = len(prices)
         res = []
-
         for i in range(n - 1):
             j = i + 1
-
             while j < n and prices[i] < prices[j]:
                 j += 1
-
             if j == n:
                 res.append(prices[i])
             else:
                 res.append(prices[i] - prices[j])
-
         res.append(prices[-1])
-
         return res
