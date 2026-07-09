bacf3bddd4b7be8c82600e14eafdd22d2f1fbf27
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-07-09 17:08:16 +0530

    Time: 19 ms (51.81%), Space: 18.9 MB (62.65%) - LeetHub

---
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        freq = {}
+        ans = 0
+
+        for a, b in dominoes:
+            key = (min(a, b), max(a, b))
+
+            ans += freq.get(key, 0)
+            freq[key] = freq.get(key, 0) + 1
+
+        return ans
