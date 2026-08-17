baad41c1c22cfb47c2104b2418b64dd7635277dd
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-08-17 18:53:28 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.78%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.py
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -6,5 +6,14 @@ class Solution(object):
                 nums[i]=0
             else:
                 nums[i]=1
-        nums.sort()
+        i = 0
+        while i < n and nums[i] != 1:
+            i += 1
+        j = i + 1
+        while j < n:
+            if nums[j] != 1:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
+
         return nums
