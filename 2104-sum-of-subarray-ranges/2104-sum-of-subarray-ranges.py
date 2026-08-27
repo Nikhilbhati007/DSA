ed694f6e939a544bf05fa7761e5af740b3ab33a7
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-08-27 19:51:24 +0530

    Time: 36 ms (60%), Space: 12.6 MB (56.43%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,15 +1,65 @@
 class Solution(object):
     def subArrayRanges(self, nums):
-        #ans=abs(subarr_min(nums)-subarr_max(nums))
-        #return ans
-        n=len(nums)
-        ans=0
-        for i in range(n):
-            mini=nums[i]
-            maxi=nums[i]
-            for j in range(i+1,n):
-                mini=min(mini,nums[j])
-                maxi=max(maxi,nums[j])
-                ans+=abs(mini-maxi)
-        return ans
+        #for subarry minmun element sum
+
+        def subarr_min(arr):
+            n = len(arr)
+            #previous smallest element 
+            stack = []
+            left = [0] * n
+            for i in range(n):
+                count = 1
+                while stack and stack[-1][0] > arr[i]:
+                    count += stack.pop()[1]
+                stack.append((arr[i], count))
+                left[i] = count
+            #next smallest element 
+            stack = []
+            right = [0] * n
+            for i in range(n-1, -1, -1):
+                count = 1
+                while stack and stack[-1][0] >= arr[i]:
+                    count += stack.pop()[1]
+                stack.append((arr[i], count))
+                right[i] = count
+            #final traversal
+            res = 0
+            for i in range(n):
+                res = (res + arr[i] * left[i] * right[i])
+            
+            return res
+
         
+        #for subarry maximun element sum
+
+        def subarr_max(arr):
+            n = len(arr)
+            #previous greatest element 
+            stack = []
+            left = [0] * n
+            for i in range(n):
+                count = 1
+                while stack and stack[-1][0] < arr[i]:
+                    count += stack.pop()[1]
+                stack.append((arr[i], count))
+                left[i] = count
+            #next greatest element 
+            stack = []
+            right = [0] * n
+            for i in range(n-1, -1, -1):
+                count = 1
+                while stack and stack[-1][0] <= arr[i]:
+                    count += stack.pop()[1]
+                stack.append((arr[i], count))
+                right[i] = count
+            #final traversal
+            res = 0
+            for i in range(n):
+                res = (res + arr[i] * left[i] * right[i])
+            
+            return res
+
+
+        #Final answer
+        ans=abs(subarr_min(nums)-subarr_max(nums))
+        return ans
