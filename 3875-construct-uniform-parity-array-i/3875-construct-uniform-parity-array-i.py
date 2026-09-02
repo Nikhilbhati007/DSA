dde673cb7887f0176dee03b47bdcf02d385f08f7
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-09-02 18:58:17 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        n=len(nums1)
+        nums2=[]
+        cnt_even=0
+        cnt_odd=0
+        for i in nums1:
+            if i%2==0:
+                cnt_even+=1
+            else:
+                cnt_odd+=1
+        if cnt_even==0 or cnt_odd==0:
+            return True
+        return True
+        
+
+
+        
