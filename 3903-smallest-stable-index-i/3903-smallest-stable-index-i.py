c7ecb4ec9ec31a48f6bd5f7b3f74185e9d595c84
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-09-04 14:32:15 +0530

    Time: 4 ms (96.3%), Space: 12.3 MB (96.3%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def firstStableIndex(self, nums, k):
+        # O(n^2)
+        '''
         n=len(nums)
         max_so_far=nums[0]
         for i in range(n):
@@ -8,6 +10,20 @@ class Solution(object):
             min_ele=min(nums[i:n])
             if max_so_far-min_ele<=k:
                 return i
-                break
         return -1
-        
+        '''
+        # O(N) solution and Sp-O(N)
+        n=len(nums)
+        max_so_far=nums[0]
+        suff_min=[0]*n
+        for i in range(n-1,-1,-1):
+            if i==n-1:
+                suff_min[i]=nums[i]
+            else:
+                suff_min[i]=min(nums[i],suff_min[i+1])
+        for i in range(n):
+            max_so_far=max(max_so_far,nums[i])
+            min_ele=suff_min[i]
+            if max_so_far-min_ele<=k:
+                return i
+        return -1
